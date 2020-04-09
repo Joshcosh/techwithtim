9a165102ff4298a0d6791be2d692fa0959d74d1c
Urish <u.shilo@gmail.com>
2020-04-09 21:05:22 +0300

    finished the video

---
+++ OOP_into.py
@@ -0,0 +1,20 @@
+import turtle
+
+x = 5
+y = 'string'
+
+print(type(x))
+print(type(y))
+print(y.strip())
+
+uri = turtle.Turtle()
+
+print(y.upper())
+print(y.replace('s', ''))
+
+
+def func(arg):
+    return arg + 1
+
+
+print(func(5))
